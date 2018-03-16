7432cb4340d00b8354c715b211606cff35983461
Pavel <darkdarin@tochka.com>
2018-03-16 15:35:31 +0500

    master: Fix migration

--- migrations/2018_03_16_180000_create_promises_table.php
+++ migrations/2018_03_16_180000_create_promises_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateEsbPromisesTable extends Migration
+class CreatePromisesTable extends Migration
 {
     /**
      * Run the migrations.
