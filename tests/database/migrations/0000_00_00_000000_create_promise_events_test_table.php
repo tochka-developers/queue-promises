d4b993c1a749f8eef8e903280d4a2b666ef28d6e
darkdarin <darkdarin@tochka.com>
2023-05-31 17:58:58 +0400

    fix: set next watch time at timeout_at for correct timeout promises

--- tests/database/migrations/0000_00_00_000000_create_promise_events_test_table.php
+++ tests/database/migrations/0000_00_00_000000_create_promise_events_test_table.php
@@ -4,14 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePromiseEventsTestTable extends Migration
+return new class () extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
         Schema::create(
             'promise_events',
@@ -28,13 +23,8 @@ class CreatePromiseEventsTestTable extends Migration
         );
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('promise_events');
     }
-}
+};
--- tests/database/migrations/0000_00_00_000000_create_promises_test_table.php
+++ tests/database/migrations/0000_00_00_000000_create_promises_test_table.php
@@ -4,38 +4,30 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePromisesTestTable extends Migration
+return new class () extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
         Schema::create(
             'promises',
             function (Blueprint $table) {
                 $table->bigIncrements('id');
+                $table->bigInteger('parent_job_id')->nullable();
                 $table->string('state');
                 $table->longText('conditions');
                 $table->longText('promise_handler');
-                $table->timestamp('watch_at');
-                $table->timestamp('timeout_at');
+                $table->timestamp('watch_at')->nullable();
+                $table->timestamp('timeout_at')->nullable();
                 $table->timestamps();
 
-                $table->index('state');
+                $table->index(['state', 'updated_at']);
+                $table->index(['state', 'watch_at']);
             }
         );
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
-        Schema::dropIfExists('{{table}}');
+        Schema::dropIfExists('promises');
     }
-}
+};
